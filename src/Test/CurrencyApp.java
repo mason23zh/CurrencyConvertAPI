e2b2c3682160be7f765e7838a421836aba387475
Zhengyang Zhao <mason.zh@gmail.com>
2016-03-23 23:54:37 -0500

    Fix project construction

--- src/Test/CurrencyApp.java
+++ src/Test/CurrencyApp.java
@@ -1,11 +1,11 @@
-package main_package.Test;
+package Test;
 
 import main_package.SetCurrencyRate;
 
 /**
  * Created by Zhengyang on 2016/3/23.
  */
-public class currencyConvertTest {
+public class CurrencyApp {
     public static void main(String[] args) {
         SetCurrencyRate setCurrencyRate = new SetCurrencyRate("CAD", "CNY");
         System.out.println(setCurrencyRate.getConvertRate());
